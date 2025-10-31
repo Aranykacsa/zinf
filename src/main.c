d1ab5b24a0262700e80f1f669921cb8070d35c52
Aranykacsa <zalanvadasz@gmail.com>
2025-10-31 13:01:46 +0100

    mod: reader and test

--- src/main.c
+++ src/main.c
@@ -24,20 +24,20 @@ int main(void) {
     for (size_t i = 0; i < sizeof(payload); i++) payload[i] = 12;
 
     printf("Writing test sector...\n");
-    uint8_t rc = save_u8bit_values(payload, sizeof(payload), &header);
+    uint8_t rc = raid_u8bit_values(payload, sizeof(payload), &header);
     if (rc != 10) {
         printf("save_u8bit_values failed (%d)\n", rc);
         return 1;
     }
 
-    printf("✅ Write OK, verifying data...\n");
-    uint8_t buffer[512];
+    printf("Write OK\n");
+    /*uint8_t buffer[512];
     active_driver->read_block(active_driver, 2, buffer);
 
     printf("First 16 bytes of sector 1: ");
     for (int i = 0; i < 16; i++)
         printf("%02X ", buffer[i]);
-    printf("\n");
+    printf("\n");*/
 
     active_driver->deinit(active_driver);
     return 0;
