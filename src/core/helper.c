1f420c1d1c1c3afd35c2dc40a7d4e07aed8f55c8
Aranykacsa <zalanvadasz@gmail.com>
2025-11-03 21:39:39 +0100

    refactor: added helper layer for crc32 and feature helpers

---
+++ src/core/helper.c
@@ -0,0 +1,16 @@
+#include "helper.h"
+
+uint32_t crc32(const uint8_t *data, size_t len) {
+    uint32_t crc = 0xFFFFFFFF;
+    for (size_t i = 0; i < len; i++) {
+        crc ^= data[i];
+        for (int j = 0; j < 8; j++) {
+            if (crc & 1)
+                crc = (crc >> 1) ^ 0xEDB88320;
+            else
+                crc >>= 1;
+        }
+    }
+    return crc ^ 0xFFFFFFFF;
+}
+
